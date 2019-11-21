355d64d5471aa26b9704b6fd9cea1d96b6d433a8
shakiz <activities335@gmail.com>
2019-11-21 18:33:53 +0600

    basic structure.

---
+++ app/src/main/java/com/shakil/tourdekuakata/MainActivity.java
@@ -0,0 +1,23 @@
+package com.shakil.tourdekuakata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        init();
+    }
+
+    private void init() {
+        bindUIWithComponents();
+    }
+
+    private void bindUIWithComponents() {
+    }
+}
---
+++ app/src/main/java/com/shakil/tourdekuakata/UX.java
@@ -0,0 +1,28 @@
+package com.shakil.tourdekuakata;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class UX {
+    private Context context;
+
+    public UX(Context context) {
+        this.context = context;
+    }
+    public void setToolbar(Toolbar toolbar, final Activity from, final Class to){
+        ((AppCompatActivity) context).setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(from, to));
+            }
+        });
+        ((AppCompatActivity) context).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_left_arrow);
+    }
+
+}
